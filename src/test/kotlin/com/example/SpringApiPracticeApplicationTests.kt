7c9e2f0df1b8c275d4b3ca6710aeb15163f144f5
ECLin <jack40062006@gmail.com>
2019-01-24 14:51:49 +0800

    Add accepting trust strategy

--- src/test/kotlin/com/example/SpringApiPracticeApplicationTests.kt
+++ src/test/kotlin/com/example/SpringApiPracticeApplicationTests.kt
@@ -1,39 +1,44 @@
 package com.example
 
+import org.apache.http.client.ClientProtocolException
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory
+import org.apache.http.impl.client.HttpClients
+import org.apache.http.ssl.SSLContexts
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpMethod
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.test.context.junit4.SpringRunner
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.net.HttpURLConnection
-import java.net.URL
+import org.springframework.web.client.RestTemplate
+import java.io.IOException
+import java.security.cert.X509Certificate
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class SpringApiPracticeApplicationTests {
 
     @Test
+    @Throws(ClientProtocolException::class, IOException::class)
     fun contextLoads() {
-        val resourceUrl = "http://gis.taiwan.net.tw/XMLReleaseALL_public/scenic_spot_C_f.json"
-        val obj = URL(resourceUrl)
 
-        with(obj.openConnection() as HttpURLConnection) {
-            requestMethod = "GET"
-            println("\n sending 'GET' request to URL : $resourceUrl")
-            println("Response Code : $responseCode")
+        val acceptingTrustStategy = { _: Array<X509Certificate>, _: String -> true }
+        val sslContext = SSLContexts.custom()
+                .loadTrustMaterial(null, acceptingTrustStategy).build()
+        val csf = SSLConnectionSocketFactory(sslContext)
 
-            BufferedReader(InputStreamReader(inputStream)).use {
-                val response = StringBuffer()
+        val resourceUrl = "https://gis.taiwan.net.tw/XMLReleaseALL_public/scenic_spot_C_f.json"
+        val httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf).build()
+        val requestFactory = HttpComponentsClientHttpRequestFactory()
+        requestFactory.httpClient = httpClient
 
-                var inputLine = it.readLine()
-                while (inputLine != null) {
-                    response.append(inputLine)
-                    inputLine = it.readLine()
-                }
-                println(response.toString())
-            }
-        }
+        val response = RestTemplate(requestFactory)
+                .exchange(resourceUrl, HttpMethod.GET, null, String::class.java)
+
+        assertThat(response.statusCode).isEqualTo(OK)
 
     }
 
